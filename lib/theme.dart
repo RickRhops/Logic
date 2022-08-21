489743fcf75c6c78a6661a1584875e530f086888
RickRhops <art.ric.sphr@gmail.com>
2022-08-21 12:14:45 -0300

    [fixed colors] [added darkGray & lightGray]

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,18 +4,19 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   //color values
   static const black = Color(0xFF000000);
-  static const gray = Color(0xFF565656);
-  static const white = Color(0xFFE6E6EB);
-  static const red = Color(0xFFff453a);
+  static const darkGray = Color(0xFF444444);
+  static const lightGray = Color(0xFF888888);
+  static const white = Color(0xFFEEEEEE);
+  static const red = Color(0xFFEE0000);
 
   //color elements
   static const background = AppColors.black;
   static const noteCardText = AppColors.white;
-  static const noteCardBorder = AppColors.gray;  
+  static const noteCardBorder = AppColors.darkGray;  
   static const newNoteInputFieldText = AppColors.white;
-  static const newNoteInputFieldHint = AppColors.gray;
+  static const newNoteInputFieldHint = AppColors.lightGray;
   static const cursorColor = AppColors.white;
-  static const secondaryText = AppColors.gray;
+  static const secondaryText = AppColors.lightGray;
   static const supportDanger = AppColors.red;
 }
 
